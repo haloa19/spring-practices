978ce63751dffcd436341e509c8629ff59e9438e
haloa19 <hj199514@gmail.com>
2020-02-21 11:47:05 +0900

    init commit (emaillist03 spring변경)

--- emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -2,10 +2,11 @@ package com.douzone.emaillist.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.douzone.emaillist.repository.EmaillistRepository;
 import com.douzone.emaillist.vo.EmaillistVo;
@@ -13,13 +14,25 @@ import com.douzone.emaillist.vo.EmaillistVo;
 @Controller
 public class EmaillistController {
 	
+	@Autowired
 	private EmaillistRepository emaillistRepository;
 	
-	@ResponseBody
 	@RequestMapping("")
 	public String index(Model model) {
 		List<EmaillistVo> list = emaillistRepository.findAll();
-		model.addAttribute("list", list);
-		return "/WEB-INF/views/index.jsp";
+		model.addAttribute("list", list);		
+		return "index";
 	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.GET)
+	public String add() {
+		return "add";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(EmaillistVo vo) {
+		emaillistRepository.insert(vo);
+		return "redirect:/";
+	}
+	
 }
