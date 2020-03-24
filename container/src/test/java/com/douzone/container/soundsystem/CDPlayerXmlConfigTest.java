538e408893e62d84e6c567448f94b90ae1965419
haloa19 <hj199514@gmail.com>
2020-03-24 16:34:43 +0900

    SoundSystem 추가

---
+++ container/src/test/java/com/douzone/container/soundsystem/CDPlayerXmlConfigTest.java
@@ -0,0 +1,38 @@
+package com.douzone.container.soundsystem;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.douzone.container.soundsystem.CDPlayer;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:config/soundsystem/CDPlayerConfig.xml")
+public class CDPlayerXmlConfigTest {
+	@Rule
+	public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+	
+	@Autowired
+	private CDPlayer cdPlayer;
+	
+	@Test
+	public void testCDPlayerNull() {
+		assertNotNull(cdPlayer);
+	}
+	
+	@Test
+	public void testPlay() {
+		cdPlayer.play();
+		assertEquals("Playing 지구멸망 by 양승호", systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
+	}
+
+}
